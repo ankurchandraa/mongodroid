3a3ddd9a6532dc926931488c7ac42c9435d571e7
Neeraj <neerajshukla@gofynd.com>
2017-07-11 18:27:17 +0530

    First commit

---
+++ app/src/main/java/bynarylab/com/mongodroid/utility/MongoConnectionHandler.kt
@@ -0,0 +1,12 @@
+package bynarylab.com.mongodroid.utility
+
+/**
+ * Created by beast on 11/7/17.
+ */
+class MongoConnectionHandler {
+
+
+    class MongoConnectionHandler constructor() {
+
+    }
+}
